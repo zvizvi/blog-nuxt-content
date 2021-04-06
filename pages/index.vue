5534c7bfa432c7e789dfb27beaf485f69e99526f
zvizvi <5633910@gmail.com>
2021-04-06 13:37:00 +0300

    Tag list style

--- pages/index.vue
+++ pages/index.vue
@@ -43,19 +43,17 @@
           </NuxtLink>
         </li>
       </ul>
-      <h3 class="mb-4 font-bold text-2xl uppercase text-center">Topics</h3>
-      <ul class="flex flex-wrap mb-4 text-center">
+      <ul class="flex flex-wrap mt-14 mb-4 text-center">
         <li
           v-for="tag of tags"
           :key="tag.slug"
-          class="xs:w-full md:w-1/3 lg:flex-1 px-2 text-center"
+          class="lg:flex-1 px-2 text-center"
         >
           <NuxtLink :to="`/tag/${tag.slug}`" class="">
-            <p
-              class="font-bold text-gray-600 uppercase tracking-wider font-medium text-ss"
+            <span
+              class="text-xs inline-block mr-2 mb-2 px-3 py-1 bg-gray-200 text-gray-800 rounded-full font-medium text-ss rounded-fullborder border-light-border dark:border-dark-border transition-colors duration-300 ease-linear"
+              >{{ tag.name }}</span
             >
-              {{ tag.name }}
-            </p>
           </NuxtLink>
         </li>
       </ul>
